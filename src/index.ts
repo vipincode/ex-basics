587cc830a072297aa5f68a6711f5f41d2073ccd0
Vipin Singh <svipin174@gmail.com>
2024-04-21 21:58:03 +0530

    Adding auth helpers functions

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
+import * as dotenv from 'dotenv';
 import app from './server';
+
+dotenv.config();
 
 app.listen(4000, () => {
   console.log('App is running on PORT: http://localhost:4000');
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,15 @@
-import express from 'express';
+import cors from 'cors';
+import express, { urlencoded } from 'express';
+import morgan from 'morgan';
+import router from './router';
+import { protect } from './modules/auth';
 
 const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   console.log('Hello from express');
@@ -8,5 +17,7 @@ app.get('/', (req, res) => {
   res.status(200);
   res.json({ message: 'Hello!' });
 });
+
+app.use('/api', protect, router);
 
 export default app;
