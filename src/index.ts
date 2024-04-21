9d852c16ec2129239d638264f0c34d461006b8e6
Vipin Singh <svipin174@gmail.com>
2024-04-21 12:44:30 +0530

    basics: 01-First server setup

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import app from './server';
+
+app.listen(4000, () => {
+  console.log('App is running on PORT: http://localhost:4000');
+});
---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  console.log('Hello from express');
+
+  res.status(200);
+  res.json({ message: 'Hello!' });
+});
+
+export default app;
